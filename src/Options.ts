844c97d9cee992ce202d5cbd585556479112a968
Enzo Innocenzi <enzo.inno@gmail.com>
2019-11-19 19:53:48 +0100

    Added comments to options

--- src/Options.ts
+++ src/Options.ts
@@ -2,9 +2,28 @@ import { IgnoreList } from "./IgnoreList";
 import { Catalogue } from "./Catalogue";
 
 export interface Options {
+  /**
+   * Use `ISO-639-1` short language codes.
+   */
   shortLanguage?: boolean;
+
+  /**
+   * Force the default locale.
+   */
   locale?: string;
+
+  /**
+   * Define a fallback locale.
+   */
   fallback?: string;
+
+  /**
+   * Override the catalogue.
+   */
   messages?: Catalogue;
+
+  /**
+   * Ignore files.
+   */
   ignore?: IgnoreList;
 }
