67f2e95126c6764c40ea9205aaec64633d53772e
Enzo Innocenzi <enzo@innocenzi.dev>
2020-06-10 02:11:45 +0200

    chore: Use `php-array-loader`
    Replace `laravel-localization-loader` by `php-array-loader`

--- mix.js
+++ mix.js
@@ -1,22 +1,21 @@
 const mix = require('laravel-mix');
 
 class LangExtension {
+	register(lang) {
+		this.lang = lang || './resources/lang';
+	}
 
-  register(lang) {
-    this.lang = lang || './resources/lang';
-  }
+	webpackRules() {
+		return {
+			test: /resources[\\\/]lang.+\.(php)$/,
+			loader: 'php-array-loader',
+		};
+	}
 
-  webpackRules() {
-    return {
-      test: /resources[\\\/]lang.+\.(php|json)$/,
-      loader: 'laravel-localization-loader',
-    }
-  }
-
-  webpackConfig(webpackConfig) {
-    webpackConfig.resolve.alias = webpackConfig.resolve.alias || {};
-    webpackConfig.resolve.alias['@lang'] = path.resolve('./resources/lang');
-  }
+	webpackConfig(webpackConfig) {
+		webpackConfig.resolve.alias = webpackConfig.resolve.alias || {};
+		webpackConfig.resolve.alias['@lang'] = path.resolve('./resources/lang');
+	}
 }
 
 mix.extend('lang', new LangExtension());
