c7e65fb9451df6b2143cdfe7ba6f466b8cb014d7
Enzo Innocenzi <enzo@innocenzi.dev>
2020-06-11 21:15:23 +0200

    feat: Add support for root-level translations (#10)
    Co-authored-by: Ruben Ruiz <rvbz@hotmail.com>

--- src/index.ts
+++ src/index.ts
@@ -44,12 +44,23 @@ function shouldIgnore(ignore: IgnoreList, locale: string, domain: string) {
 /**
  * Imports translations from the configured alias.
  */
-function importTranslations({ ignore }: Partial<Options>): Translations {
+function importTranslations({ ignore, globalTranslationsKey }: Partial<Options>): Translations {
 	const catalogue: Translations = {};
 	const files = require.context('@lang', true, /\.(php|json)$/);
 
 	files.keys().forEach((file: string) => {
-		const [match, locale, domain] = new RegExp('./(.*)/(.*).(?:php|json)').exec(file) ?? [];
+		// Find localization files at the root directory
+		const [isGlobal, rootLocale] = /\.\/([A-Za-z0-9-_]+).(?:php|json)/.exec(file) ?? [];
+
+		if (isGlobal) {
+			catalogue[`${rootLocale}.${globalTranslationsKey}`] = files(file);
+
+			return;
+		}
+
+		// Find localization files in a /lang/ directory
+		const [isScoped, locale, domain] =
+			/\.\/([A-Za-z0-9-_]+)\/([A-Za-z0-9-_]+).(?:php|json)/.exec(file) ?? [];
 
 		if (!ignore || !shouldIgnore(ignore, locale, domain)) {
 			catalogue[`${locale}.${domain}`] = files(file);
@@ -80,6 +91,12 @@ declare module 'vue/types/vue' {
  */
 const Lang = {
 	install: (Vue: VueConstructor, options: Partial<Options> = {}) => {
+		// Defines default options
+		options = {
+			globalTranslationsKey: '__global__',
+			...options,
+		};
+
 		// Creates the Lang.js object
 		const i18n = new Translator({
 			fallback: document.documentElement.lang || navigator.language,
@@ -87,8 +104,20 @@ const Lang = {
 			...options,
 		});
 
-		// Defines the translation function
-		const __: TranslateFunction = (...args) => i18n.get(...args);
+		// Defines a global translation function
+		const __: TranslateFunction = (key, ...args) => {
+			// Non-global translations
+			if (key.match(/^[\w-]+(?:\.[\w-]+)+$/)) {
+				return i18n.get(key, ...args);
+			}
+
+			// Global translations
+			const result = i18n.get(`${options.globalTranslationsKey}.${key}`, ...args);
+
+			return result.startsWith(options.globalTranslationsKey!)
+				? result.substr(options.globalTranslationsKey!.length + 1)
+				: result;
+		};
 
 		Vue.mixin({
 			methods: {
